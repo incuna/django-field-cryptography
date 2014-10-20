2f2ceccf92bd49059af42e8b3cf5bc4fb4049e53
Kevin Etienne <kevin@incuna.com>
2014-10-20 09:41:25 +0100

    Add SubfieldBase metaclass

--- django_field_cryptography/fields.py
+++ django_field_cryptography/fields.py
@@ -1,25 +1,33 @@
 from django.conf import settings
 from django.db import models
 from django.utils.encoding import force_bytes, force_str
+from django.utils.six import with_metaclass
 
-from cryptography.fernet import Fernet
+from cryptography.fernet import Fernet, InvalidToken
 
 
 fernet = Fernet(settings.FERNET_KEY)
 
 
-class EncryptedTextField(models.TextField):
+class EncryptedTextField(with_metaclass(models.SubfieldBase, models.TextField)):
     """A TextField encrypted with Fernet (AES).
 
     EncryptedTextField rely on `Fernet` from `cryptography` to ensure symetric
     encryption. This field is compatible with South migrations.
     """
+    def db_type(self, connection):
+        return 'bytea'
+
     def get_prep_value(self, value):
         return fernet.encrypt(force_bytes(value))
 
     def to_python(self, value):
-        decrypted = fernet.decrypt(value)
-        return force_str(decrypted)
+        try:
+            value = fernet.decrypt(force_bytes(value))
+        except InvalidToken:
+            return value
+        else:
+            return force_str(value)
 
     def south_field_triple(self):
         """Returns a suitable description of this field for South."""
--- django_field_cryptography/tests/test_fields.py
+++ django_field_cryptography/tests/test_fields.py
@@ -3,7 +3,7 @@
 from django.test import TestCase
 
 from .. import fields
-from . import factories
+from . import factories, models
 
 
 class TestField(TestCase):
@@ -31,5 +31,6 @@ class TestField(TestCase):
 class TestModel(TestCase):
     def test_value(self):
         value = 'Encrypt me'
-        dummy_model = factories.DummyModelFactory.create(aes_field=value)
+        factories.DummyModelFactory.create(aes_field=value)
+        dummy_model = models.DummyModel.objects.get()
         self.assertEqual(dummy_model.aes_field, value)
